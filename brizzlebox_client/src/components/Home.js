fd81e1893d5f8f6d7e437668f704f14a72f76f7f
JameswDemps <jameswdemps@gmail.com>
2019-11-21 12:02:50 +0000

    Coded my own css javascript

--- brizzlebox_client/src/components/Home.js
+++ brizzlebox_client/src/components/Home.js
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
 
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
 import "./css/Home.css";
 
 export default class Home extends React.Component {
   state = {
     dimensions: 0,
-    link: "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrizzlebox%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+    link:
+      "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrizzlebox%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
   };
 
   componentDidMount() {
     var width = 0;
     if (this.container.offsetWidth < 500) {
       width = this.container.offsetWidth;
-    }
-    else {
+    } else {
       width = 500;
     }
 
     this.setState({
       dimensions: {
+        trueWidth: this.container.offsetWidth,
         width: width,
         height: this.container.offsetHeight
       },
-      link: "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrizzlebox%2F&tabs=timeline&width="+width+"&height="+this.container.offsetHeight+"&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId",
+      link:
+        "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrizzlebox%2F&tabs=timeline&width=" +
+        width +
+        "&height=" +
+        this.container.offsetHeight +
+        "&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
     });
   }
 
@@ -51,8 +59,67 @@ export default class Home extends React.Component {
     window.open("https://www.youtube.com/channel/UC7-2MKKVFNDp9XN_3Stp7IA");
   }
 
+  myMove = () => {
+    console.log("myMove Called")
+    var elem = document.getElementsByClassName("frame_container_animationv2")[0]   
+    var pos = 0;
+    var frameRate = 5;
+    var delay = 2000;
+
+    var id = setInterval(left, frameRate);
+    console.log(elem.style.left)
+    function sleep(miliseconds) {
+      var currentTime = new Date().getTime();
+   
+      while (currentTime + miliseconds >= new Date().getTime()) {
+      }
+   }
+
+    function left() {
+      if (pos == -100) {
+        clearInterval(id);
+        sleep(delay);
+        id = setInterval(right, frameRate);
+      } else {
+        pos -= 0.5; 
+        elem.style.left = pos + '%'; 
+      }
+    }
+    function right() {
+
+      // console.log("frame")
+      if (pos == 0) {
+        clearInterval(id);
+        sleep(delay);
+        id = setInterval(left, frameRate);
+      } else {
+        // console.log("pos: " + pos)
+        pos += 0.5; 
+        // elem.style.top = pos + '%'; 
+        elem.style.left = pos + '%'; 
+      }
+    }
+  }  
+
+  frame_container_animationv2 = {
+    height: "100%",
+    width: "200%",
+    overflow: "hidden",
+  
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    position: "relative"
+  }
+
+  // componentDidMount() {
+    // this.myMove()
+  // }
+
   render() {
     const { dimensions, link } = this.state;
+
     return (
       <div className="Home">
         <div className="Home-header">Brizzlebox</div>
@@ -116,24 +183,39 @@ export default class Home extends React.Component {
             </div>
             <div className="frame_container" ref={el => (this.container = el)}>
               {/* <div className="Hello">{dimensions && this.renderContent()}</div> */}
+              <div style={this.frame_container_animationv2} className="frame_container_animationv2">
+                <div className="frame_container_item">
+                  <iframe
+                    className="facebook_frame"
+                    src={this.state.link}
+                    // width="420px !important"
+                    width={this.state.dimensions.width}
+                    // height="500"
+                    // height = "100%"
 
-              <iframe
-                className="facebook_frame"
-                src={this.state.link}
-                // width="420px !important"
-                width={this.state.dimensions.width}
-                // height="500"
-                // height = "100%"
+                    // style="border:none;overflow:hidden"
+                    scrolling="no"
+                    frameborder="0"
+                    allowTransparency="true"
+                    allow="encrypted-media"
+                  ></iframe>
+                </div>
 
-                // style="border:none;overflow:hidden"
-                scrolling="no"
-                frameborder="0"
-                allowTransparency="true"
-                allow="encrypted-media"
-              ></iframe>
+                <div className="frame_container_item">
+                  <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="brizzlebox"
+                    options={{
+                      height: this.state.dimensions.height,
+                      width: this.state.dimensions.trueWidth
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        <button onClick={this.myMove}>Click Me</button> 
         {/* <div className="container">
             <div className="row">
               <div className="col-sm-6 cold-md-6 c "> Hey</div>
