6d420af4268d0efb5923c2c2b66f6d6ff52cc3c0
JameswDemps <JameswDemps@github.com>
2019-11-30 11:56:18 +0000

    Removed mess from Home.js

--- brizzlebox_client/src/components/Home.js
+++ brizzlebox_client/src/components/Home.js
@@ -1,18 +1,4 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-
-import {
-  TwitterTimelineEmbed,
-  TwitterShareButton,
-  TwitterFollowButton,
-  TwitterHashtagButton,
-  TwitterMentionButton,
-  TwitterTweetEmbed,
-  TwitterMomentShare,
-  TwitterDMButton,
-  TwitterVideoEmbed,
-  TwitterOnAirButton
-} from "react-twitter-embed";
 
 import "./css/Home.css";
 
@@ -20,24 +6,6 @@ import "./css/Home.css";
 import Carousel from "./Carousel";
 
 export default class Home extends React.Component {
-  state = {
-    dimensions: 0,
-    link:
-      "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrizzlebox%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-  };
-
-
-  renderContent() {
-    const { dimensions } = this.state;
-
-    return (
-      <div>
-        width: {dimensions.width}
-        <br />
-        height: {dimensions.height}
-      </div>
-    );
-  }
 
   openFacebook() {
     window.open("https://www.facebook.com/brizzlebox/");
@@ -52,119 +20,7 @@ export default class Home extends React.Component {
     window.open("https://www.youtube.com/channel/UC7-2MKKVFNDp9XN_3Stp7IA");
   }
 
-  myMove = () => {
-    console.log("myMove Called");
-
-    var elem = document.getElementsByClassName(
-      "frame_container_animationv2"
-    )[0];
-
-    elem.style.width =
-      document.getElementsByClassName("frame_container_item").length + "00%";
-
-    var noOfItems = document.getElementsByClassName("frame_container_item")
-      .length;
-    var currentItem = 1;
-    var moveLeft = false;
-
-    var step = 0;
-    var pos = 0;
-    var posStep = 1;
-    var frameRate = 10;
-    var delay = 2000;
-
-    var percentProgress = [];
-
-    var noOfSteps = 100 / posStep;
-    erf();
-
-    var id = setInterval(right, frameRate);
-
-    function erf() {
-      var spacing = 4.5 / noOfSteps;
-      var firstHalf = [];
-      var secondHalf = [];
-      var progressBar = [];
-      var a_1 = 0.278393;
-      var a_2 = 0.230389;
-      var a_3 = 0.000972;
-      var a_4 = 0.078108;
-      for (var i = 0; i < noOfSteps / 2; i++) {
-        var x = i * spacing;
-        var newPercent =
-          (1 -
-            1 /
-              (1 + a_1 * x + a_2 * x ** 2 + a_3 * x ** 3 + a_4 * x ** 4) ** 4) *
-          50;
-        firstHalf.push(50 - newPercent);
-        secondHalf.push(newPercent + 50);
-      }
-      progressBar = firstHalf.reverse().concat(secondHalf);
-      for (var i = 0; i < progressBar.length; i++) {
-        percentProgress.push(progressBar[i + 1] - progressBar[i]);
-      }
-    }
-
-    function right() {
-      if (step == noOfSteps - 2) {
-        clearInterval(id);
-        step = 0;
-        setTimeout(function(){ 
-          if (currentItem == noOfItems - 1) {
-            moveLeft = true;
-          }
-          if (moveLeft) {
-            currentItem--;
-            id = setInterval(left, frameRate);
-          } else {
-            currentItem++;
-            id = setInterval(right, frameRate);
-          }
-        }, delay);
-      } else {
-        step++;
-        pos -= percentProgress[step];
-        elem.style.left = pos + "%";
-      }
-    }
-    function left() {
-      if (step == noOfSteps - 2) {
-        clearInterval(id);
-        step = 0;
-        setTimeout(function(){ 
-          if (currentItem == 0) {
-            moveLeft = false;
-          }
-          if (moveLeft) {
-            currentItem--;
-            id = setInterval(left, frameRate);
-          } else {
-            currentItem++;
-            id = setInterval(right, frameRate);
-          }
-         }, delay);
-      } else {
-        step++;
-        pos += percentProgress[noOfSteps - 2 - step];
-        elem.style.left = pos + "%";
-      }
-    }
-  };
-
-  frame_container_animationv2 = {
-    height: "100%",
-    width: "100%",
-    overflow: "hidden",
-
-    flex: 1,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    position: "relative"
-  };
-
   render() {
-    const { dimensions, link } = this.state;
 
     return (
       <div className="Home">
